798dd204cbfda502a87cf2513fe46b89fef4fc48
“Jessica <osias.jessica@gmail.comgit config --global user.email osias.jessica@gmail.com>
2024-06-06 15:55:02 -0400

    recommited

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -11,26 +11,15 @@ namespace ChristmasWithTypes
             var xmasDay = Christmas.Day.Thursday;
 
             //TODO set Santa's name to Kris Kringle
-            string santaName = "Kris Kringle"
-            xmas.Santa = null;
+            string santaName = "Kris Kringle";
+            xmas.Santa = santaName;
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
             xmas.Presents =  new string[3]{"sandals", "bracelet", "makeup" };
 
             //TODO Set the TreeHeight to 10
             int treeHeight = 10;
-            xmas.TreeHeight = null;
-
-            Console.WriteLine($"This year christmas falls on {xmasDay} \n");
-            Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
-            Console.WriteLine("Here are the presents we would like:");
-
-            foreach (var present in xmas.Presents)
-            {
-                Console.WriteLine($"    {present}");
-            }
-
-            Console.WriteLine($"\n We like to call Santa, {xmas.Santa}");
+            xmas.TreeHeight = treeHeight;
         }
     }
 }
